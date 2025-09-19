7a6d4914dc28a83dcff017a3a403978bbbf562f7
viyash-24 <viyashviyashan2417@gmail.com>
2025-09-19 19:08:12 +0530

    react library instalazion

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import PaymentPage from './pages/PaymentPage';
+import AdminDashboardPage from './pages/AdminDashboardPage';
+import ParkingAreasPage from './pages/ParkingAreasPage';
+import NotFoundPage from './pages/NotFoundPage';
+import ProtectedRoute from './routes/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Toaster position="top-center" reverseOrder={false} />
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/admin" element={
+              <ProtectedRoute requireAdmin>
+                <AdminDashboardPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/admin/parking-areas" element={
+              <ProtectedRoute requireAdmin>
+                <ParkingAreasPage />
+              </ProtectedRoute>
+            } />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
