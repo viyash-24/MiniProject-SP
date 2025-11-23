5f383735ec98d6dce9c540e34d7e97fe1d370fd3
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-23 19:53:18 +0530

    ui design is modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,7 +45,7 @@ function App() {
   return (
     <Router>
       <LoadingProvider>
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-100 transition-colors duration-300">
           <Toaster position="top-center" reverseOrder={false} />
           <LoadingBar />
           <NavigationHandler />
