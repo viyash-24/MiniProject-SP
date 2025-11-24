fe34e1760e0636c920985c59b33cf4db64ace3be
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-24 19:35:46 +0530

    Sign In function is updated

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -14,4 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 export const db = getFirestore(app);
