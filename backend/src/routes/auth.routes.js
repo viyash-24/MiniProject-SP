47744708727c8660b6ef1e03bb9121b02c39765b
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-25 09:31:26 +0530

    Entrollment Email function controller is route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, me, register, logout, updateProfile } from '../controllers/auth.controller.js';
+import { login, me, register, logout, updateProfile, enrollEmail } from '../controllers/auth.controller.js';
 import { authRequired } from '../middleware/auth.js';
 
 const r = Router();
@@ -9,5 +9,6 @@ r.post('/login', login);
 r.get('/me', authRequired, me);
 r.put('/me', authRequired, updateProfile);
 r.post('/logout', authRequired, logout);
+r.post('/enroll-email', enrollEmail);
 
 export default r;
