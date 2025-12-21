24ab69579585a046c0e14ce26c80091f46e1d29c
Udara Rathnayake <166277555+sandu2098@users.noreply.github.com>
2025-12-21 14:18:58 +0530

    Change comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 // TODO: Replace with your own Stripe publishable key
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || 'pk_test_51RmDSjPrn4yDohxSkl5YV6mJzkaSPclh2x8CLIezDjDieRPyDAs65Le8T39u2EVIfzT9Ee3bCObxKfAcU7X4uJlg00Le36dWLJ');
 
+// Render the application with AuthProvider, ThemeProvider, and Stripe Elements
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
