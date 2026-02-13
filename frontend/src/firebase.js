038c92bb445d80a7801644ab5fb186df1976ace1
WMD Tharushika <166277302+WMDTHARUSHIKA@users.noreply.github.com>
2026-02-13 14:53:44 +0530

    change some code lines

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,9 +1,9 @@
-// Firebase v9+ modular SDK
+
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Replace with your Firebase project config
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'AIzaSyCmkiOOv53tqLrCK8b0EedFkFbMErdn1EY',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'smart-parking-a80b9.firebaseapp.com',
