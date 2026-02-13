bd173cd745ec2d8172f6e6b047c8dccc31b098af
WMD Tharushika <166277302+WMDTHARUSHIKA@users.noreply.github.com>
2026-02-13 14:53:14 +0530

    change code lines

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,8 @@ import { AuthProvider } from './context/AuthContext';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { ThemeProvider } from "./context/ThemeContext";
-// TODO: Replace with your own Stripe publishable key
+
+
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || 'pk_test_51RmDSjPrn4yDohxSkl5YV6mJzkaSPclh2x8CLIezDjDieRPyDAs65Le8T39u2EVIfzT9Ee3bCObxKfAcU7X4uJlg00Le36dWLJ');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
