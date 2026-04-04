2db8937b51a636fbc46d1c28f40372073b43536e
Viyash@24 <viyashviyashan2417@gmail.com>
2026-04-04 15:01:44 +0530

    Delete test route from auth.routes.js
    Removed the test route for authentication.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -11,7 +11,5 @@ r.put('/me', authRequired, updateProfile);
 r.post('/logout', authRequired, logout);
 r.post('/enroll-email', enrollEmail);
 
-r.get('/test', (req, res) => {
-  res.send("Auth route working");
-});
+
 export default r;
